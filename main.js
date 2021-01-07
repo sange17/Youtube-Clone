5c5fd4633d3e52e1c0fabd78f3e8ebe95cb72bc1
TSKwon <sange17@naver.com>
2021-01-08 02:52:56 +0900

    added disign 0108

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const morBtn = document.querySelector('.info .metadata .moreBtn');
+const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
 
-morBtn.addEventListener('click', () => {
+moreBtn.addEventListener('click', () => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
-})
+});
